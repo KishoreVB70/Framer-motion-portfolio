a4249fa05b17fec3290fb127eb90885aa931b6dc
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-17 09:03:15 +0530

    added required divs and imported motion section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+import { motion } from "framer-motion"; 
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
-      
-
+    <motion.section className="grid grid-cols-3 p-10 gap-10">
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+      <div className="bg-slate-800 aspect-square rounded-lg flex items-center justify-center gap-10" ></div>
+    </motion.section>
     </div>
 
   );
