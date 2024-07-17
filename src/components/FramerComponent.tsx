de830cc3f799d2acd53b35050e6643522182dcfe
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-17 22:06:32 +0530

    holy moly, became a pro in scroll animation, I myself I itself I kishore by myself without a single refernce did a reverse scroll animation just using my understanding, holy molly of jesus

--- src/components/FramerComponent.tsx
+++ src/components/FramerComponent.tsx
@@ -6,6 +6,7 @@ import HoverButton from "./HoverButton";
 import DragAndDrop from "./DragAndDrop";
 import ScrollProgress from "./ScrollProgress";
 import SVG from "./SVG";
+import MoreScroll from "./MoreScroll";
 
 export default function FramerComponent() {
   const parentVariants = {
@@ -29,18 +30,20 @@ export default function FramerComponent() {
     }
   }
   return (
-    <motion.section className="grid grid-cols-3 p-10 gap-10"
-      variants={parentVariants}
-      initial="hidden"
-      animate="show"
-    >
-      <FadeIn childVariants={childVariants} />
-      <ScaleAndRotate childVariants={childVariants} />
-      <HoverButton childVariants={childVariants} />
-      <DragAndDrop childVariants={childVariants} />
-      <ScrollProgress childVariants={childVariants} />
-      <SVG childVariants={childVariants}/>
-      
-    </motion.section>
+    <>
+      <motion.section className="grid grid-cols-3 p-10 gap-10"
+        variants={parentVariants}
+        initial="hidden"
+        animate="show"
+        >
+        <FadeIn childVariants={childVariants} />
+        <ScaleAndRotate childVariants={childVariants} />
+        <HoverButton childVariants={childVariants} />
+        <DragAndDrop childVariants={childVariants} />
+        <ScrollProgress childVariants={childVariants} />
+        <SVG childVariants={childVariants}/>
+      </motion.section>
+      <MoreScroll />
+    </>
   );
 }
--- src/components/MoreScroll.tsx
+++ src/components/MoreScroll.tsx
@@ -1,9 +1,57 @@
 "use client";
-import React from 'react'
+import {motion, useAnimation, useInView, useScroll, useTransform } from 'framer-motion';
+import React, { useEffect, useRef } from 'react'
 
 const MoreScroll = () => {
+    // For the header
+    const containerReference = useRef(null);
+    const isInView = useInView(containerReference, {once: true})
+    const animation = useAnimation()
+
+    // For para
+    const {scrollYProgress} = useScroll({
+        target: containerReference,
+        offset: ["start end", "end start"]
+    })
+    const p1Value = useTransform(scrollYProgress, [0,0.5,0.8, 1], ["-100%", "0%", "0%", "-100%"])
+    const p2Value = useTransform(scrollYProgress, [0,0.5,0.8, 1], ["100%", "0%", "0%", "100%"])
+
+    // For header
+    useEffect(() => {
+        if (isInView) {
+            animation.start("visible");
+        }
+    }, [isInView]);
+
   return (
-    <div>MoreScroll</div>
+    <>
+        <div className='flex flex-col gap-10 mb-10' ref={containerReference}>
+            <motion.h1 className='text-5xl tracking-wide text-slate-100 text-center'
+                initial="hidden"
+                animate={animation} 
+                variants={{
+                    hidden: {opacity: 0, y: 75},
+                    visible: {opacity:1, y:0}
+                }}
+                transition={{delay: 0.3}}
+            >
+                Keep scrolling
+            </motion.h1>
+            <motion.p className='text-slate-100 font-thin text-4xl w-1/2 mx-auto'
+                style={{translateX: p1Value}}
+            >
+                falsdkjf kahsdkjfhasdkj laksdfjlkasjdf ruioewurio jfalskjflkasj rio23uroiwu jaslfkjaslkdf asdf iuoyoiuy  asfas fa
+            </motion.p>
+            <motion.p className='text-slate-100 font-thin text-4xl w-1/2 mx-auto'
+                style={{translateX: p2Value}}
+            >
+                falsdkjf kahsdkjfhasdkj laksdfjlkasjdf ruioewurio jfalskjflkasj rio23uroiwu jaslfkjaslkdf asdf iuoyoiuy  asfas fa
+            </motion.p>
+        </div>
+        <div className='h-[100vh]'>
+
+        </div>
+    </>
   )
 }
 
